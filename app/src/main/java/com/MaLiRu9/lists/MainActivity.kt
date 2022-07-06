7740ea07f3c61f71ac8ff3c1bf45cf42985a9743
MaLiRu9 <lolo_fosils_93@yahoo.es>
2022-07-06 13:47:26 +0200

    feat: sixth of July

--- app/src/main/java/com/MaLiRu9/lists/MainActivity.kt
+++ app/src/main/java/com/MaLiRu9/lists/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.MaLiRu9.lists
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.MaLiRu9.lists.list.ListActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, ListActivity::class.java).apply {
+            Log.d("Debug", "Starting List Activity")
+        }
+        startActivity(intent)
     }
 }
